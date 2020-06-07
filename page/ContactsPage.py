2922c423f28cf81e466f46191c7f9f3520d3348e
micheng <micheng005@gmail.com>
2020-06-07 17:08:41 +0800

    新增通讯录滑动

--- page/ContactsPage.py
+++ page/ContactsPage.py
@@ -2,6 +2,7 @@
 # -*- coding:UTF-8 -*-
 from time import sleep
 from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
 from selenium.webdriver.common.by import By
 
 from page.StartPage import StartAction
@@ -9,7 +10,7 @@ from page.StartPage import StartAction
 
 class ContactsList:
     contacts_button = (By.ID, "com.showfires.im:id/iv_contacts")
-    friend_list = (By.XPATH, "//*[@text='aa1234']")
+    friend_list = (By.XPATH, "//*[@text='神仙鱼']")
     talk_button = (By.XPATH, "//*[@text='对话']")
 
     def __init__(self, appium_driver):
@@ -28,10 +29,23 @@ class ContactsList:
     def talk_button_action(self):
         self.driver.find_element(*self.talk_button).click()
 
+    # 滑动页面
+    def scroll(self):
+        size = self.driver.get_window_size()
+        for i in range(3):
+            TouchAction(self.driver) \
+                .long_press(x=size['width'] * 0.5, y=size['height'] * 0.8) \
+                .move_to(x=size['width'] * 0.5, y=size['height'] * 0.2) \
+                .release() \
+                .perform()
+
     def talk_action(self):
         sleep(2)
         self.contacts_button_action()
         sleep(2)
+        self.scroll()
+        sleep(5)
         self.friend_list_action()
         sleep(2)
         self.talk_button_action()
+
--- page/StartPage.py
+++ page/StartPage.py
@@ -8,38 +8,38 @@ class StartAction:
 
     # 向左滑动屏幕
     def swipe_left(self, n=1, t=500):
-        l = self.driver.get_window_size()
-        x1 = l['width'] * 0.75
-        y1 = l['height'] * 0.5
-        x2 = l['width'] * 0.2
+        size = self.driver.get_window_size()
+        x1 = size['width'] * 0.75
+        y1 = size['height'] * 0.5
+        x2 = size['width'] * 0.2
         for i in range(n):
             self.driver.swipe(x1, y1, x2, y1, t)
 
     # # 向右滑动屏幕
     # def swipe_Right(self, n=1):
-    #     l = self.driver.get_window_size()
-    #     x1 = l['width'] * 0.25
-    #     y1 = l['height'] * 0.5
-    #     x2 = l['width'] * 0.75
+    #     size = self.driver.get_window_size()
+    #     x1 = size['width'] * 0.25
+    #     y1 = size['height'] * 0.5
+    #     x2 = size['width'] * 0.75
     #     self.driver.swipe(x1, y1, x2, y1)
     #
     #
     # # 向上滑动屏幕
     # def swipe_up(self, n=1, t=500):
-    #     l = self.driver.get_window_size()
-    #     x1 = l['width'] * 0.5
-    #     y1 = l['height'] * 0.75
-    #     y = l['height'] * 0.25
+    #     size = self.driver.get_window_size()
+    #     x1 = size['width'] * 0.5
+    #     y1 = size['height'] * 0.75
+    #     y = size['height'] * 0.25
     #     for i in range(n):
     #         self.driver.swipe(x1, y1, x1, y, t)
     #
     #
     # # 向上滑动屏幕
     # def swipe_Down(self, n=1):
-    #     l = self.driver.get_window_size()
-    #     x1 = l['width'] * 0.5
-    #     y1 = l['height'] * 0.25
-    #     y = l['height'] * 0.75
+    #     size = self.driver.get_window_size()
+    #     x1 = size['width'] * 0.5
+    #     y1 = size['height'] * 0.25
+    #     y = size['height'] * 0.75
     #     self.driver.swipe(x1, y1, x1, y)
 
     # def swipe_On(self, direction):
